601b6753543dd3a02dac5a999088ceae2db5e303
Michael Yee <michaeljoshuayee@gmail.com>
2022-01-23 01:29:59 -0500

    files in views/* compile to json with checksum for each file

--- giftcard-app/scripts/deploy_views.js
+++ giftcard-app/scripts/deploy_views.js
@@ -1,7 +1,9 @@
+const crypto = require('crypto');
+const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
-const { exec } = require('child_process');
+const secret = 'veryimportantsecret';
 
 const viewsDir = `${__dirname}/../views/`;
 const outputFile = `${__dirname}/../dist/views.json`;
@@ -12,10 +14,17 @@ if(fs.existsSync(outputFile)) fs.unlinkSync(outputFile);
 fs.readdir(viewsDir, function(err, files){
   if(err) console.error("failed to get files", err);
   else{
-    files.forEach(file => {
-      const key = file.split(".pug").join("");
-      const filePath = `${viewsDir}${file}`;
+    files.forEach(filename => {
+      const filePath = `${viewsDir}${filename}`;
       const value = pug.renderFile(filePath, {});
+      const sha = crypto.createHmac("sha256", secret);
+      const checksum = sha.update(value).digest("hex");
+
+      console.log(filename, checksum);
+
+      /* filename + checksum */
+      const key = filename.split(".pug").join(`_${checksum}`);
+
       const mb = Buffer.byteLength(value, 'utf8') * 0.000001;
 
       if(mb > 25) console.log(`${filePath} HAS EXCEEDED 25MB!`);
