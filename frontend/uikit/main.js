0861799e8b1ee0850093b201c31bbd71551a4053
Mike Ball <mike.ball3@gmail.com>
2022-03-17 08:11:52 -0400

    in with new

---
+++ frontend/uikit/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import VueTilt from "vue-tilt.js";
+import VueSweetalert2 from "vue-sweetalert2";
+import MaterialDashboard from "./material-dashboard";
+
+const appInstance = createApp(App);
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(VueTilt);
+appInstance.use(VueSweetalert2);
+appInstance.use(MaterialDashboard);
+appInstance.mount("#app");
