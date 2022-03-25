d8aeebc5a604abf22c724c0ae7b6c5fa12409950
Mike Ball <mike.ball3@gmail.com>
2022-03-25 14:17:03 -0400

    working.... probably needs await/async treatment

--- scratch.mjs
+++ scratch.mjs
@@ -1,8 +1,12 @@
-import {User} from './db/db.mjs';
+import 'dotenv/config';
 
-async function main(){
-  let err, ret = await User.findById(1);
-  console.log("result", err, ret);  
+//import {User} from './db/db.mjs';
+import {sendTextEmail} from "./lib/email.mjs";
+
+function main(){
+  sendTextEmail("mike.ball3@gmail.com", "Did this work", "hello nurse!", function(err, ret){
+    console.log("err", err, ret);
+  });
 }
 
 main();
