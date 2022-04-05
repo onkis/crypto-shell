7b3e9144f58b688371cf2009b006993809d74a73
Michael Yee <michaeljoshuayee@gmail.com>
2022-04-04 22:40:41 -0400

    fetching donation script

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,7 +1,10 @@
 import * as Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router.js';
 import App from './App.vue';
 
 Vue.createApp(App)
+.use(VueAxios, axios)
 .use(router)
 .mount('#app');
--- frontend/app/pages/Setup.vue
+++ frontend/app/pages/Setup.vue
@@ -10,23 +10,47 @@ main.main-content.mt-0
             .bg-gradient-success.shadow-success.border-radius-lg.py-3.pe-1.text-center.py-4
               h4.font-weight-bolder.text-white.mt-1 Setup
               p.mb-1.text-sm.text-white Configure Your Donation Button
-          .card-body HELLO WORLD {{ name }}
-</template>
+          .card-body
+            form.text-start(role="form", action="/auth/login", method="post")
+              .input-group.input-group-static.mb-2
+                label Donation Address
+                input#donationAddress.form-control(v-model="config.address" onchange="update()" type='text' placeholder='Donation Address')
+              .input-group.input-group-static.mb-2
+                label Label
+                input#label.form-control(v-model="config.label" onchange="update()" type='text' placeholder='Label')
+              .input-group.input-group-static.mb-2
+                label Redirect Url
+                input#redirectUrl.form-control(v-model="config.redirectUrl" onchange="update()" type='text' placeholder='Redirect Url')
+              code#script(rows='6' readOnly='true').form-control.d-sm-flex.align-items-center.bg-gray-100.border-radius-lg.p-2.my-4.is-filled
+                pre.
+                  &lt;script&gt;
+                    (function(d, src){
+                      var e = d.createElement('script');e.src = src;
+                      d.querySelector('head').appendChild(e);
+                    })(document, 'http://localhost:3000/x?id=3');
+                  &lt;/script&gt;
+  </template>
 
 <script>
 export default {
   components: {},
   data() {
     return {
-      name: "jimmy"
+      config: {}
     };
   }, 
   mounted() {
     this.init();
   },
   methods: {
-    init(){
-      console.log("init");
+    async init(){
+      const response = await this.$http("/api/assets/3");
+      
+      const { data } = response;
+
+      this.id = data.id;
+      this.config = { ...data.config };
+      console.log(this.config);
     }
   }
 }
