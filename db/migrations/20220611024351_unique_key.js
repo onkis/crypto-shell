d73fae0fd9b7af8cfaf1d28b40d7c5786ca3629d
Mike Ball <mike.ball3@gmail.com>
2022-06-10 22:47:04 -0400

    needed this

---
+++ db/migrations/20220611024351_unique_key.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('kvstore', function(table) {
+    table.unique('key')
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('kvstore', function(table){
+    table.dropUnique('key');
+  });
+};
