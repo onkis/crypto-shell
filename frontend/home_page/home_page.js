96910014bf232e7b52d9f363251b73b3ca0cadc2
Mike Ball <mike.ball3@gmail.com>
2022-07-10 15:17:09 -0400

    remove un-used code

--- frontend/home_page/home_page.js
+++ frontend/home_page/home_page.js
@@ -3,27 +3,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import HomePage from './HomePage.vue';
 
-// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-// import { BraveWalletAdapter } from "@solana/wallet-adapter-brave";
-// import { CoinbaseWalletAdapter } from "@solana/wallet-adapter-coinbase";
-// import { ExodusWalletAdapter } from "@solana/wallet-adapter-exodus";
-// import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-// import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
-// 
-// import SolanaWallets from 'solana-wallets-vue';
-// 
-// const walletOptions = {
-//   wallets: [
-//     new BraveWalletAdapter({ network: WalletAdapterNetwork.Testnet }),
-//     new PhantomWalletAdapter({ network: WalletAdapterNetwork.Testnet }),
-//     new SolflareWalletAdapter({ network: WalletAdapterNetwork.Testnet })
-//   ],
-//   autoConnect: true
-// }
-// 
-// import 'solana-wallets-vue/styles.css';
-
 Vue.createApp(HomePage)
 .use(VueAxios, axios)
-// .use(SolanaWallets, walletOptions)
 .mount('#HomePage');
