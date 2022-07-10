5eb400058e331b3a0566af4e517323e584f92f89
Michael Yee <michaeljoshuayee@gmail.com>
2022-07-09 21:18:56 -0400

    displaying some info on payment history pay

--- frontend/app/pages/PaymentHistory.vue
+++ frontend/app/pages/PaymentHistory.vue
@@ -17,8 +17,10 @@
               th Memo
               th Signature
           tbody
-            tr
-
+            tr(v-for="tx in transactions")
+              td {{ tx.transaction_ref_id }}
+              td {{ tx.created_at }}
+              td {{ tx.amount }}
 </template>
 
 <script>
@@ -28,7 +30,8 @@ export default {
   components: {},
   data() {
     return {
-      config: {}
+      config: {},
+      transactions: []
     };
   }, 
   mounted() {
@@ -36,11 +39,16 @@ export default {
   },
   methods: {
     async init(){
+      const that = this;
       const response = await this.$http("/api/transactions");
       const { data } = response;
-      console.log("response", response);
+      console.log(data);
+      const transactions = [];
       //https://github.com/fiduswriter/Simple-DataTables/blob/main/docs/9-fetch-api/index.html
       //TODO: this Data Table is lgpl...probably needs to be changed...
+      that.transactions = [...data];
+
+
       const dataTableSearch = new DataTable("#transactions", {
         searchable: true,
         fixedHeight: false,
@@ -57,6 +65,7 @@ export default {
           data: data
         }
       });
+
     }
   }
 }
