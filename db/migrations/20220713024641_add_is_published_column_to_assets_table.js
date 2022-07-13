63b850d01548dc9dfb0d64a7366c409c6e2f85a5
Michael Yee <michaeljoshuayee@gmail.com>
2022-07-12 22:55:48 -0400

    is published to paymentPages table

---
+++ db/migrations/20220713024641_add_is_published_column_to_assets_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('paymentpages', table => {
+    table.boolean('is_published');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('paymentpages', table => {
+    table.dropColumn('is_published');
+  });
+};
