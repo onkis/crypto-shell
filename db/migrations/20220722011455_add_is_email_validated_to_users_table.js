b60584bd4f80502e2f8f59b96846ebb2a39be609
Michael Yee <michaeljoshuayee@gmail.com>
2022-07-21 21:30:54 -0400

    add column "is_email_validated" to "users" table

---
+++ db/migrations/20220722011455_add_is_email_validated_to_users_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('users', table => {
+    table.boolean('is_email_validated');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('users', table => {
+    table.dropColumn('is_email_validated');
+  });
+};
