985a0f67dbb9b30d9c11c9ea222ce29075b40a09
Mike Ball <mike.ball3@gmail.com>
2022-08-04 14:31:40 -0400

    fix crasher

--- api/paymentpage.mjs
+++ api/paymentpage.mjs
@@ -7,7 +7,7 @@ export async function create(req, res){
 
   const [err, paymentPage] = await PaymentPage.create({...newRecord});
   if(err){
-    console.error(`failed to create paymentPage | ${__filename}#create`, err);
+    console.error(`failed to create paymentPage | paymentpage.mjs#create`, err);
     return res.send(500);
   }
 
@@ -21,7 +21,7 @@ export async function get(req, res){
 
   const [err, paymentPage] = await PaymentPage.findOne({ org_id });
   if(err){
-    console.error(`failed to get paymentPage | ${__filename}#get`, err);
+    console.error(`failed to get paymentPage | paymentpage.mjs#get`, err);
     return res.send(500);
   }
 
@@ -41,7 +41,7 @@ export async function update(req, res){
 
   const [err, assets] = await PaymentPage.update({ where, update });
   if(err){
-    console.error(`failed to update paymentPage | ${__filename}#update`, err);
+    console.error(`failed to update paymentPage | paymentpage.mjs#update`, err);
     return res.send(500);
   }
 
@@ -52,7 +52,7 @@ export async function destroy(req, res){
   const id = req.params.id;
   const [err, assets] = await PaymentPage.destroy({ id });
   if(err){
-    console.error(`failed to destroy paymentPage | ${__filename}#destroy`, err);
+    console.error(`failed to destroy paymentPage | paymentpage.mjs#destroy`, err);
     return res.send(500);
   }
 
@@ -66,7 +66,7 @@ export async function publish(req, res){
 
   [err, user] = await User.findOne({ org_id, is_email_validated: true });
   if(err){
-    console.error(`failed to get user | ${__filename}#publish`, err);
+    console.error(`failed to get user | paymentpage.mjs#publish`, err);
     return res.status(500).send();
   }
   else if(!user){
@@ -75,7 +75,7 @@ export async function publish(req, res){
 
   [err, paymentPage] = await PaymentPage.findOne({ org_id, eula });
   if(err){
-    console.error(`failed to get paymentPage | ${__filename}#publish`, err);
+    console.error(`failed to get paymentPage | paymentpage.mjs#publish`, err);
     return res.status(500).send();
   }
   else if(!paymentPage){
@@ -84,7 +84,7 @@ export async function publish(req, res){
 
   [err, assets] = await _setPublishedState(org_id, is_published);
   if(err){
-    console.error(`failed to get assets | ${__filename}#publish`, err);
+    console.error(`failed to get assets | paymentpage.mjs#publish`, err);
     return res.send(500);
   }
 
@@ -97,7 +97,7 @@ export async function unpublish(req, res){
 
   const [err, assets] = await _setPublishedState(org_id, is_published);
   if(err){
-    console.error(`failed to unpublish | ${__filename}#unpublish`, err);
+    console.error(`failed to unpublish | paymentpage.mjs#unpublish`, err);
     return res.send(500);
   }
 
