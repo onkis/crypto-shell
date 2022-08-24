aee406a8b9169c5c1242484b69a4ac8f961b2d95
Mike Ball <mike.ball3@gmail.com>
2022-08-24 10:53:41 -0400

    move app code around

---
+++ frontend/old-app/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+// Page Navigation?
+aside.sidenav.navbar.navbar-vertical.navbar-expand-xs.border-0.border-radius-xl.my-3.fixed-start.ms-3.bg-gradient-dark
+  navbar
+main.main-content.mt-0
+  alerts
+  router-view
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Alerts from './components/Alerts.vue';
+export default {
+  components: { Alerts, Navbar },
+  data() {
+    return {};
+  }, 
+  mounted() {},
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/old-app/router.js
@@ -0,0 +1,33 @@
+import {createWebHashHistory, createRouter} from 'vue-router';
+import Setup from './pages/Setup.vue';
+import UserSettings from './pages/UserSettings.vue'
+import PaymentHistory from './pages/PaymentHistory.vue'
+const routes = [
+  {
+    path: "/user-setttings",
+    name: "UserSettings",
+    component: UserSettings
+  },
+  {
+    path: "/payment-history",
+    name: "PaymentHistory",
+    component: PaymentHistory
+  },
+  {
+    path: "/payment-setup",
+    name: "PaymentSetup",
+    component: Setup 
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/payment-setup"
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory("/app"),
+  routes,
+  linkExactActiveClass: 'active'
+});
+
+export default router;
