90a596afff9fda907f3bbc4890db4abf7f5575b4
Mike Ball <mike.ball3@gmail.com>
2022-08-24 10:53:41 -0400

    move app code around

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,27 +1,28 @@
 import {createWebHashHistory, createRouter} from 'vue-router';
-import Setup from './pages/Setup.vue';
-import UserSettings from './pages/UserSettings.vue'
-import PaymentHistory from './pages/PaymentHistory.vue'
+// import Setup from './pages/Setup.vue';
+// import UserSettings from './pages/UserSettings.vue'
+// import PaymentHistory from './pages/PaymentHistory.vue'
+
 const routes = [
-  {
-    path: "/user-setttings",
-    name: "UserSettings",
-    component: UserSettings
-  },
-  {
-    path: "/payment-history",
-    name: "PaymentHistory",
-    component: PaymentHistory
-  },
-  {
-    path: "/payment-setup",
-    name: "PaymentSetup",
-    component: Setup 
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    redirect: "/payment-setup"
-  }
+  // {
+  //   path: "/user-setttings",
+  //   name: "UserSettings",
+  //   component: UserSettings
+  // },
+  // {
+  //   path: "/payment-history",
+  //   name: "PaymentHistory",
+  //   component: PaymentHistory
+  // },
+  // {
+  //   path: "/payment-setup",
+  //   name: "PaymentSetup",
+  //   component: Setup 
+  // },
+  // {
+  //   path: "/:pathMatch(.*)*",
+  //   redirect: "/payment-setup"
+  // }
 ];
 
 const router = createRouter({
