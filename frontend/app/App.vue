73c9aa9ca541ed40471842bbd2669e1a9020c3fa
Mike Ball <mike.ball3@gmail.com>
2022-08-25 17:23:10 -0400

    added a TINY rotipay logo

--- frontend/app/App.vue
+++ frontend/app/App.vue
@@ -14,7 +14,7 @@ div
                   XIcon.h-6.w-6.text-white(aria-hidden='true')
             .flex-1.h-0.pt-5.pb-4.overflow-y-auto
               .flex-shrink-0.flex.items-center.px-4
-                img.h-8.w-auto(src='https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600', alt='Workflow')
+                img.h-8.w-auto(src='/images/rotipay-logo-purple.gif', alt='Workflow')
               nav.mt-5.px-2.space-y-1
                 a(v-for='item in navigation', :key='item.name', :href='item.href', :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']")
                   component(:is='item.icon', :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-4 flex-shrink-0 h-6 w-6']", aria-hidden='true')
@@ -27,7 +27,7 @@ div
     .flex-1.flex.flex-col.min-h-0.border-r.border-gray-200.bg-white
       .flex-1.flex.flex-col.pt-5.pb-4.overflow-y-auto
         .flex.items-center.flex-shrink-0.px-4
-          img.h-8.w-auto(src='https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600', alt='Workflow')
+          img.h-8.w-auto(src='/images/rotipay-logo-purple.gif', alt='Workflow')
         nav.mt-5.flex-1.px-2.bg-white.space-y-1
           a(v-for='item in navigation', :key='item.name', :href='item.href', :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']", @click="navChange(item)")
             component(:is='item.icon', :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']", aria-hidden='true')
