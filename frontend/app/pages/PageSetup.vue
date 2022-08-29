10fc1cfee4ad9506cd1189671111f219ee8a6427
Michael Yee <michaeljoshuayee@gmail.com>
2022-08-29 01:30:15 -0400

    modal implemented in separate file

--- frontend/app/pages/PageSetup.vue
+++ frontend/app/pages/PageSetup.vue
@@ -1,30 +1,5 @@
 <template lang="pug">
 #setup-page.px-8
-  TransitionRoot(as="template" :show="openValidateEmailModal")
-    Dialog(as="div" @close="openValidateEmailModal = false" class="relative z-10")
-      TransitionChild(as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0")
-        div(class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity")
-      div(class="fixed z-10 inset-0 overflow-y-auto")
-        div(class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0")
-          TransitionChild(as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95")
-            DialogPanel(class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6")
-              div
-                .text-center
-                  DialogTitle(as="h3" class="text-lg leading-6 font-medium text-gray-900")
-                    h5.modal-title.font-weight-normal Send Validate Email
-                  div(class="mt-4")
-                    p(class="text-sm text-gray-500") Must validate email before publishing roti page
-                  hr.my-2
-                  .mt-4
-                    input(
-                      v-model="config.email"
-                      type='email'
-                      placeholder='Email'
-                      class='focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                    ).shadow-sm.block.w-full.border-gray-300.rounded-md
-              div(class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense")
-                button(@click="openValidateEmailModal = false" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-1 sm:text-sm") Cancel
-                button(@click="openValidateEmailModal = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-2 sm:text-sm") Validate Email
   .container-fluid.py-4
     .row
       .col-lg-4
@@ -135,17 +110,19 @@
               a(:href="landingPageLink" target='_blank') {{ landingPageLink }}
             hr.my-2
             iframe#preview(v-bind:src="previewLink" style='width:140%; height:140%; transform: scale(0.7); transform-origin: 0; margin-top:-20%')
+  ValidateEmailModal(
+    :modalActive="openValidateEmailModal"
+    @close="validateEmailModalClosed"
+  )
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-
 const openValidateEmailModal = ref(false);
 </script>
 
 <script>
-//import ValidateEmailModal from '../modals/ValidateEmailModal.vue';
+import ValidateEmailModal from '../modals/ValidateEmailModal.vue';
 //import EulaModal from '../modals/EulaModal.vue';
 import { debounce, isEmpty } from "lodash";
 export default {
@@ -216,9 +193,11 @@ export default {
     async publish(){
       const response = await this.$http.post("/api/paymentpage/3/publish");
       if(response?.data?.VALIDATE_EMAIL){
+        /* Must Validate Email  */
         this.openValidateEmailModal = true;
       }
       else if(response?.data?.EULA){
+        /* Must Agree To Terms */
         this.openEulaModal = true;
       }
       else if(response){
@@ -251,12 +230,10 @@ export default {
       document.getElementById("fileUpload").click();
     },
     async validateEmailModalClosed(modalResults){
-      this.openValidateEmailModal = false;
+      this.openValidateEmailModal = false; /* CLOSE MODAL */
       if(modalResults?.eventType !== 'success') return;
       else if(!modalResults?.email?.length) return console.error("no email");
-      else{
-        await this.$http.post("/api/user/validate_email", { email: modalResults.email });
-      }
+      else await this.$http.post("/api/user/validate_email", { email: modalResults.email });
     },
     async eulaModalClosed(modalResults){
       this.openEulaModal = false;
