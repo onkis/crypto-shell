450263076e96ea170917f5cde2d6b578c62b7853
Mike Ball <mike.ball3@gmail.com>
2022-08-30 22:34:20 -0400

    fix buttons

--- frontend/app/modals/ValidateEmailModal.vue
+++ frontend/app/modals/ValidateEmailModal.vue
@@ -23,7 +23,7 @@ TransitionRoot(as="template" :show="openValidateEmailModal")
                   ).shadow-sm.block.w-full.border-gray-300.rounded-md
             div(class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense")
               button(@click="handleCancel" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-1 sm:text-sm") Cancel
-              button(@click="handleValidate" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-2 sm:text-sm") Validate Email
+              button(@click="handleValidate" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-2 sm:text-sm") Validate Email
 </template>
 
 <script setup>
